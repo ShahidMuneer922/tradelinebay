e9265693102b355cd1f95239f67a8f24a7367184
ShahidMuneer922 <wwwshahidmuneer1@gmail.com>
2021-07-02 19:19:07 +0500

    hack it

--- chatting/consumers.py
+++ chatting/consumers.py
@@ -1,6 +1,5 @@
-from channels.consumer import SyncConsumer, AsyncConsumer
-from asgiref.sync import async_to_sync, sync_to_async
-# from django.contrib.auth.models import User
+from channels.consumer import AsyncConsumer
+from asgiref.sync import sync_to_async
 from .models import Thread, Message
 from trade.models import User
 import json
@@ -8,18 +7,11 @@ from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
-        a = self.scope['headers'][4]
-        print(type(a))
-        head = a[0]
-        auth = head.decode('utf-8')
-        print(auth)
-        if auth == 'authorization':
-            data = a[1]
-            print(data.decode("utf-8"))
-            username = data.decode("utf-8")
-            me = await sync_to_async(User.objects.get)(id=username)
-            # print(me)
+            me = self.scope['url_route']['kwargs']['user']
+            print(me)
+            me = await sync_to_async(User.objects.get)(id=me)
             other_username = self.scope['url_route']['kwargs']['username']
+            print(other_username)
             other_user = await sync_to_async(User.objects.get)(id=other_username)
             # print(other_user)
             self.thread_obj = await sync_to_async(Thread.objects.get_or_create_personal_thread)(me, other_user)
@@ -31,62 +23,19 @@ class ChatConsumer(AsyncConsumer):
                 'type': 'websocket.accept',
             })
             print(f'[{self.channel_name}] - you are connected')
-        a = self.scope['headers'][0]
-        head = a[0]
-        auth = head.decode('utf-8')
-        print(auth)
-        if auth == 'authorization':
-            data = a[1]
-            print(data.decode("utf-8"))
-            username = data.decode("utf-8")
-            me = await sync_to_async(User.objects.get)(id=username)
-            # print(me)
-            other_username = self.scope['url_route']['kwargs']['username']
-            other_user = await sync_to_async(User.objects.get)(id=other_username)
-            # print(other_user)
-            self.thread_obj = await sync_to_async(Thread.objects.get_or_create_personal_thread)(me, other_user)
-            # print(self.thread_obj)
-            self.room_name = "chat-%s" % self.thread_obj.id
-            # print(self.room_name)
-            await self.channel_layer.group_add(self.room_name, self.channel_name)
-            await self.send({
-                'type': 'websocket.accept',
-            })
-            print(f'[{self.channel_name}] - you are connected')
-
 
 
     async def websocket_receive(self, event):
         print(f'[{self.channel_name}] - Received message - {event["text"]}')
-        a = self.scope['headers'][0]
-        head = a[0]
-        auth = head.decode('utf-8')
-        print(auth)
-        if auth == 'authorization':
-            data = a[1]
-            print(data.decode("utf-8"))
-            username = data.decode("utf-8")
-            msg = json.dumps(
-                {
-                    'type': event.get('text'),
-                    'text': str(User.objects.get(id=username)), }
-            )
+        me = self.scope['url_route']['kwargs']['user']
 
-        a = self.scope['headers'][4]
-        head = a[0]
-        auth = head.decode('utf-8')
-        print(auth)
-        if auth == 'authorization':
-            data = a[1]
-            print(data.decode("utf-8"))
-            username = data.decode("utf-8")
-            msg = json.dumps(
-                {
-                    'type': event.get('text'),
-                    'text': str(User.objects.get(id=username)), }
-            )
-        print(msg)
-
+        msg = json.dumps(
+            {
+                'type': event.get('text'),
+                'text': str(User.objects.get(id=me)), }
+        )
+        # print(msg)
+        print(event.get('text'))
         await self.store_message(event.get('text'))
 
         await self.channel_layer.group_send(
@@ -102,7 +51,7 @@ class ChatConsumer(AsyncConsumer):
         print(f'[{self.channel_name}] - message sent - {event["text"]}')
         await self.send({
             'type': 'websocket.send',
-            'text': event.get('text'),
+            'text': event["text"],
         }
         )
 
@@ -112,30 +61,9 @@ class ChatConsumer(AsyncConsumer):
 
     @database_sync_to_async
     def store_message(self, text):
-        a = self.scope['headers'][4]
-        head = a[0]
-        auth = head.decode('utf-8')
-        print(auth)
-        if auth == 'authorization':
-            data = a[1]
-            print(data.decode("utf-8"))
-            username = data.decode("utf-8")
-            Message.objects.create(
-                thread=self.thread_obj,
-                sender=User.objects.get(id=username),
-                text=text
-            )
-        a = self.scope['headers'][0]
-        print(type(a))
-        head = a[0]
-        auth = head.decode('utf-8')
-        print(auth)
-        if auth == 'authorization':
-            data = a[1]
-            print(data.decode("utf-8"))
-            username = data.decode("utf-8")
-            Message.objects.create(
-                thread=self.thread_obj,
-                sender=User.objects.get(id=username),
-                text=text
-            )
+        me = self.scope['url_route']['kwargs']['user']
+        Message.objects.create(
+            thread=self.thread_obj,
+            sender=User.objects.get(id=me),
+            text=text
+        )
--- chatting/routing.py
+++ chatting/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<username>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<username>\w+)/(?P<user>\w+)/$', ChatConsumer.as_asgi()),
     ]
