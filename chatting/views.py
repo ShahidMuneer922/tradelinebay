af84e79747a172dbd49fee871ab6ffa7603011db
ShahidMuneer922 <wwwshahidmuneer1@gmail.com>
2021-07-07 16:40:59 +0500

    fas

--- chatting/views.py
+++ chatting/views.py
@@ -6,36 +6,39 @@ from .models import Message
 
 @api_view(['GET'])
 def previous_msg(request, pk, id):
-    my_list =[]
-    your_list = []
-    # print(list(Message.objects.filter(sender=me).values('text')))
-    data = list(Message.objects.filter(sender=pk).values('thread'))
-    dat = list(Message.objects.filter(sender=id).values('thread'))
-    for i in data and dat:
-        print(i)
-        thread = str(i)
-        da = thread.strip("{'thread':}")
-    print(da)
-    user = list(Message.objects.filter(thread=da).values('text', "time", 'sender'))
-    print(user)
+    try:
+        my_list =[]
+        your_list = []
+        # print(list(Message.objects.filter(sender=me).values('text')))
+        data = list(Message.objects.filter(sender=pk).values('thread'))
+        dat = list(Message.objects.filter(sender=id).values('thread'))
+        for i in data and dat:
+            thread = str(i)
+            da = thread.strip("{'thread':}")
 
-    #     thread1 = i
-    #     print(thread1)
-    # print(thread1)
-    # print("next")
-    # for d in dat:
-    #     thread2 = d
-    #     print(thread2)
-    #     if thread1 == thread2:
-    #         user = Message.objects.filter(thread=thread2).values('text', "time", 'sender')
-    #         for b in user:
-    #             # print(i)
-    #             my_list.append(b)
+        user = list(Message.objects.filter(thread=da).values('text', "time", 'sender'))
+        print(user)
 
-    # print(data, dat)
+        #     thread1 = i
+        #     print(thread1)
+        # print(thread1)
+        # print("next")
+        # for d in dat:
+        #     thread2 = d
+        #     print(thread2)
+        #     if thread1 == thread2:
+        #         user = Message.objects.filter(thread=thread2).values('text', "time", 'sender')
+        #         for b in user:
+        #             # print(i)
+        #             my_list.append(b)
 
-    return Response({'data': user})
+        # print(data, dat)
 
+        return Response({'data': user})
+    except Exception as e:
+        message = str(e)
+        return Response({"success":False,
+                         "message":message})
 
 
 
